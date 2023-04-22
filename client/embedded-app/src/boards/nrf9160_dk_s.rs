584f681c463c27c9c68671ac557b9dc8153ef727
huntc <huntchr@gmail.com>
2023-04-23 07:34:13 +1000

    Update Embassy
    Particularly with the new bind_interrupts approach.

--- client/embedded-app/src/boards/nrf9160_dk_s.rs
+++ client/embedded-app/src/boards/nrf9160_dk_s.rs
@@ -1,21 +1,24 @@
-use embassy_nrf::{interrupt, peripherals, Peripherals};
+use embassy_nrf::{bind_interrupts, peripherals, uarte, Peripherals};
 
-pub type NetworkUarte = peripherals::UARTETWISPI0;
-pub type NetworkUarteInterrupt = interrupt::UARTE0_SPIM0_SPIS0_TWIM0_TWIS0;
+pub type NetworkUarte = peripherals::SERIAL0;
 pub type NetworkUarteRxPin = peripherals::P0_12;
 pub type NetworkUarteTxPin = peripherals::P0_10;
 
+bind_interrupts!(pub struct NetworkPeripheralsIrqs {
+    UARTE0_SPIM0_SPIS0_TWIM0_TWIS0 => uarte::InterruptHandler<peripherals::SERIAL0>;
+});
+
 pub struct NetworkPeripherals {
     pub uarte: NetworkUarte,
-    pub uarte_interrupt: NetworkUarteInterrupt,
+    pub uarte_interrupt: NetworkPeripheralsIrqs,
     pub uarte_rx_pin: NetworkUarteRxPin,
     pub uarte_tx_pin: NetworkUarteTxPin,
 }
 
 pub fn init(p: Peripherals) -> NetworkPeripherals {
     NetworkPeripherals {
-        uarte: p.UARTETWISPI0,
-        uarte_interrupt: interrupt::take!(UARTE0_SPIM0_SPIS0_TWIM0_TWIS0),
+        uarte: p.SERIAL0,
+        uarte_interrupt: NetworkPeripheralsIrqs,
         uarte_rx_pin: p.P0_12,
         uarte_tx_pin: p.P0_10,
     }
--- server/embedded-app/src/boards/microbit_v2.rs
+++ server/embedded-app/src/boards/microbit_v2.rs
@@ -1,13 +1,16 @@
-use embassy_nrf::{interrupt, peripherals, Peripherals};
+use embassy_nrf::{bind_interrupts, peripherals, uarte, Peripherals};
 
 pub type NetworkUarte = peripherals::UARTE0;
-pub type NetworkUarteInterrupt = interrupt::UARTE0_UART0;
 pub type NetworkUarteRxPin = peripherals::P0_04;
 pub type NetworkUarteTxPin = peripherals::P0_02;
 
+bind_interrupts!(pub struct NetworkPeripheralsIrqs {
+    UARTE0_UART0 => uarte::InterruptHandler<peripherals::UARTE0>;
+});
+
 pub struct NetworkPeripherals {
     pub uarte: NetworkUarte,
-    pub uarte_interrupt: NetworkUarteInterrupt,
+    pub uarte_interrupt: NetworkPeripheralsIrqs,
     pub uarte_rx_pin: NetworkUarteRxPin,
     pub uarte_tx_pin: NetworkUarteTxPin,
 }
@@ -15,7 +18,7 @@ pub struct NetworkPeripherals {
 pub fn init(p: Peripherals) -> NetworkPeripherals {
     NetworkPeripherals {
         uarte: p.UARTE0,
-        uarte_interrupt: interrupt::take!(UARTE0_UART0),
+        uarte_interrupt: NetworkPeripheralsIrqs,
         uarte_rx_pin: p.P0_04,
         uarte_tx_pin: p.P0_02,
     }
